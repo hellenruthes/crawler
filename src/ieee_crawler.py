fbe55631ab37bb367a77b65dc6fe60fb5fd8c79f
hellenruthes <hellenruthes@gmail.com>
2022-03-02 16:12:02 -0300

    added code to arrive until UTFPR login

--- src/ieee_crawler.py
+++ src/ieee_crawler.py
@@ -1,25 +1,39 @@
+import time
+import logging
 from selenium import webdriver
 from settings import chrome_drive_path
-import time
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
 
 
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(
-    executable_path=chrome_drive_path,
-    chrome_options=options
-    )
+def _main():
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(
+        executable_path=chrome_drive_path,
+        chrome_options=options
+        )
 
-#driver.get('https://www-periodicos-capes-gov-br.ez48.periodicos.capes.gov.br/index.php?')
-time.sleep(10)
+    sleeping_and_logging(3)
+
+    driver.get('https://www-periodicos-capes-gov-br.ezl.periodicos.capes.gov.br/index.php/acesso-cafe.html')
+    sleeping_and_logging(2)
+    
+    logging.warning("Looking for element id select2-selection__rendered and clicking on it")
+    driver.find_element_by_class_name('select2-selection__rendered').click()
+    sleeping_and_logging(1)
+
+    logging.warning("Writing university name")
+    driver.find_element_by_class_name('select2-search__field').send_keys("UTFPR - UNIVERSIDADE TECNOLOGICA FEDERAL DO PARANA")
+    driver.find_element_by_class_name('select2-search__field').send_keys(Keys.RETURN)
+    sleeping_and_logging(1)
+    
+    driver.find_element_by_id('enviarInstituicaoCafe').click()
+    sleeping_and_logging(4)
 
 
-#driver.get('https://www-periodicos-capes-gov-br.ez48.periodicos.capes.gov.br/index.php/acesso-cafe.html')
-#driver.get('https://shibboleth.utfpr.edu.br/idp/profile/SAML2/Redirect/SSO?execution=e2s1')
-driver.get('https://www-periodicos-capes-gov-br.ezl.periodicos.capes.gov.br/index.php/acesso-cafe.html')
-time.sleep(30)
-instituicao = driver.find_element_by_id("select2-listaInstituicoesCafe-container")
+def sleeping_and_logging(time_in_seconds):
+    logging.warning("Sleeping for {} second(s)...".format(time_in_seconds))
+    time.sleep(time_in_seconds)
+    
 
-instituicao.send_keys("utfpr")
-#password.send_keys("Pa55worD")
-
-#driver.find_element_by_name("submit").click()
+_main()
